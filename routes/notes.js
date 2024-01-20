0a82ac68bf95c520ac3f0e14c46900e106b0b6a1
hjenp22 <hjipark22@gmail.com>
2024-01-20 17:52:19 -0600

    //post route to add a new note

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+let notes = [];
+
+//get route to get all the notes
+router.get('/', (req, res) => {
+    res.json(notes);
+});
+
+//post route to add a new note
+router.post('/', (req, res) =>{
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const newNote = { title, text};
+        notes.push(newNote);
+        res.json(newNote);
+    } else {
+        res.status(400).json({error:'Title and texts are required!'});
+    }
+});
+
