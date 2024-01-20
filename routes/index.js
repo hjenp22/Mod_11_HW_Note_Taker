9f1fe4bc485e07890fb9be9ea91f0b473e8712fa
hjenp22 <hjipark22@gmail.com>
2024-01-20 17:36:47 -0600

    post route to add a new note

--- routes/index.js
+++ routes/index.js
@@ -8,3 +8,15 @@ router.get('/notes', (req,res) => {
     res.json(notes);
 });
 
+//post route to add a new note
+router.post('/notes', (req, res) => {
+    if(title && text) {
+        const newNote = {title, text};
+        notes.push(newNote);
+        res.json(newNote);
+    } else {
+        res.status(400).json({error: 'Title and text are required'});
+    }
+});
+
+
