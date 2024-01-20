8d06fe897f3c00eb9cf700699c2638ee9fc97b33
hjenp22 <hjipark22@gmail.com>
2024-01-20 17:40:05 -0600

    delete route to delete a note by index

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,15 @@ router.post('/notes', (req, res) => {
     }
 });
 
+//delete route to delete a note by index
+router.delete('/notes/:index', (req, res) => {
+    const index = parseInt(req.params.ubdex);
+    if (!isNaN(index) && index >= 0 && index < notes.length) {
+        const deletedNote = notes.splice(index, 1);
+        res.json(deletedNote);
+    } else {
+        res.setMaxListeners(400).json({ error:'Invalid index.'});
+    }
+});
 
+module.exports = router;
