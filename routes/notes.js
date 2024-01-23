8d8216cc9a19531fff1c6d999ac6a2adf44fa697
hjenp22 <hjipark22@gmail.com>
2024-01-23 10:13:37 -0600

    trying to make work

--- routes/notes.js
+++ routes/notes.js
@@ -2,43 +2,41 @@ const notes = require('express').Router();
 const fs = require('fs');
 const uuid = require('uuid');
 
-
-
-//get route to get all the notes
-notes.get('/', (req, res)=> {
-    let notesData = fs.readFileSync("./db/db.json", "utf8");
-    notesData = JSON.parse(notesData);
-    res.json(notesData);
+// get route to get all the notes
+notes.get('/', (req, res) => {
+  let notesData = fs.readFileSync('./db/db.json', 'utf8');
+  notesData = JSON.parse(notesData);
+  res.json(notesData);
 });
 
-//post route to add a new note
-notes.post('/', (req, res) =>{
-    const {title, text} = req.body;
+// post route to add a new note
+notes.post('/', (req, res) => {
+  const { title, text } = req.body;
 
-    if (req.body) {
-        const newNote = { 
-            title, 
-            text,
-            id: uuid.v4(),
-        };
-        let notesData = fs.readFileSync("./db/db.json", "utf8");
-        notesData.push(newNote);
-        fs.writeFileSync('./db/db.json', JSON.stringify(notesData), 'utf8');
-        res.json('note was added');
-    } else {
-        res.error('error when adding note');
-    }
-});
-
-//delete route to delete a note by index
-notes.delete('/:id', (req, res)=>{
-    let notesData = fs.readFileSync("./db/db.json", "utf8");
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid.v4(),
+    };
+    let notesData = fs.readFileSync('./db/db.json', 'utf8');
     notesData = JSON.parse(notesData);
-
-    const filteredNotes = notesData.filter((note) => req.params.id !== note.id)
+    notesData.push(newNote);
     fs.writeFileSync('./db/db.json', JSON.stringify(notesData), 'utf8');
-    res.json('note was deleted');
-   
+    res.json('note was added');
+  } else {
+    res.error('error when adding note');
+  }
+});
+
+// delete route to delete a note by id
+notes.delete('/:id', (req, res) => {
+  let notesData = fs.readFileSync('./db/db.json', 'utf8');
+  notesData = JSON.parse(notesData);
+
+  const filteredNotes = notesData.filter((note) => req.params.id !== note.id);
+  fs.writeFileSync('./db/db.json', JSON.stringify(filteredNotes), 'utf8');
+  res.json('note was deleted');
 });
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const path = require('path');
-const notesRouter = require('./routes/notes.js');
+const apiRouter = require('./routes'); 
 
-
-
-const PORT  = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/notes', notesRouter);
-app.use('/api', api);
+app.use('/api', apiRouter);
 
-//ROUTES!
-//get route for the notes.html
-app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+// ROUTES!
+// get route for the notes.html
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-//get route for index page
+// get route for the index page
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, 'public', '/index.html'))
 );
+
 
 app.listen(PORT, () =>
-    console.log(`App is listening at http://localhost:${PORT}`)
+  console.log(`App is listening at http://localhost:${PORT}`)
 );
